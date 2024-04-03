32bce03978df740e193d2aa338b41d2f9b57f060
Dennis <dennis@omni.network>
2024-04-03 16:03:19 +0200

    test(unit): add functions

--- src/token/xERC20.sol
+++ src/token/xERC20.sol
@@ -5,11 +5,27 @@ import "../../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "../../lib/omni/contracts/src/pkg/XApp.sol";
 
 contract xERC20 is ERC20, XApp {
+    mapping(uint64 => address) public chainToContract;
+    uint64[] public chainIds;
+    address public admin;
+
     constructor(
         string memory name,
         string memory symbol,
-        address portal
-    ) ERC20(name, symbol) XApp(portal) {} // deploy with 0 tokens
+        address portal,
+        address _admin
+    ) ERC20(name, symbol) XApp(portal) {
+        // deploy with 0 tokens
+        admin = _admin; // admin allowed to update the mapping
+    }
+
+    /// @dev Receive mapping updates from main chain
+    function setChainAddress(uint64 chainId, address contractAddress) external xrecv {
+        require(isXCall(), "xERC20: only xcall");
+        require(xmsg.sender == admin, "xERC20: only admin");
+        chainToContract[chainId] = contractAddress;
+        chainIds.push(chainId);
+    }
 
     /// @dev Receive tokens from another chain
     function xreceive(
